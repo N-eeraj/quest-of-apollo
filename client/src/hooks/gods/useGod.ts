2ed15024ce6e7bcc52d322341bf70f4c59df5b49
N-eeraj <neeraj7rajeena@gmail.com>
2025-12-08 22:07:08 +0530

    god view page

---
+++ client/src/hooks/gods/useGod.ts
@@ -0,0 +1,44 @@
+import {
+  useParams,
+} from "react-router";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+import type { God } from "@/types";
+
+const GET_GOD = gql`
+  query God($id: ID!) {
+    god(id: $id) {
+      id
+      name
+      domains
+      relations {
+        id
+        hero {
+          id
+          name
+        }
+        relation
+      }
+    }
+  }
+`;
+
+export default function useGod() {
+  const {
+    id,
+  } = useParams<{ id: string }>();
+
+  const {
+    loading,
+    data,
+  } = useQuery<{ god: God }>(GET_GOD, {
+    variables: {
+      id,
+    },
+  });
+
+  return {
+    loading,
+    data,
+  }
+}
--- client/src/pages/gods/[id]/index.tsx
+++ client/src/pages/gods/[id]/index.tsx
@@ -1,6 +1,105 @@
+import {
+  Link,
+} from "react-router";
+import Stack from "@mui/material/Stack";
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import useGod from "@hooks/gods/useGod";
+
 function God() {
+  const {
+    loading,
+    data,
+  } = useGod();
+
   return (
-    <div>God</div>
+    <>
+      {loading && <LinearProgress color="secondary" />}
+      {data && (
+        <Stack
+          rowGap={{
+            xs: 1,
+            sm: 1.5,
+          }}
+          paddingX={{
+            xs: 2,
+            sm: 3,
+            md: 4,
+          }}
+          paddingY={1}>
+          <Typography
+            variant="h3"
+            component="h1"
+            fontWeight={700}>
+            {data.god.name}
+          </Typography>
+
+          <Stack rowGap={{
+            xs: 0.5,
+          }}>
+            <Typography
+              variant="h5"
+              component="h3">
+              God of:-
+            </Typography>
+            <Box
+              component="ul"
+              sx={{
+                paddingLeft: {
+                  xs: 3,
+                  sm: 4,
+                },
+              }}>
+              {data.god.domains.map((domain, index) => (
+                <Typography
+                  key={index}
+                  component="li"
+                  fontWeight={600}>
+                  {domain}
+                </Typography>
+              ))}
+            </Box>
+          </Stack>
+
+          <Stack rowGap={{
+            sm: "2px",
+          }}>
+            <Typography
+              variant="h5"
+              component="h3">
+              Relations
+            </Typography>
+            <Stack
+              component="ul"
+              sx={{
+                listStyle: "none",
+              }}>
+              {data.god.relations.map(({ id, hero, relation }) => (
+                <Typography
+                  key={id}
+                  component="li">
+                  {relation} of&nbsp;
+                  <Typography
+                    component={Link}
+                    to={`/heroes/${hero.id}`}
+                    sx={{
+                      textDecoration: "none",
+                      fontWeight: 600,
+                      color: "inherit",
+                      ":hover": {
+                        textDecoration: "underline",
+                      },
+                    }}>
+                    {hero.name}
+                  </Typography>
+                </Typography>
+              ))}
+            </Stack>
+          </Stack>
+        </Stack>
+      )}
+    </>
   )
 }
 
